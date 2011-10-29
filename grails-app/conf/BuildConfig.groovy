5a943e48c23b28eaf24f99b36396c88569a601b7
David Dawson <david.dawson@dawsonsystems.com>
2011-10-29 23:14:03 +0100

    a

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -2,8 +2,6 @@ grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
 
-grails.plugin.location.deployHelper="../DeployHelper"
-
 grails.server.port.http=8090
 
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
